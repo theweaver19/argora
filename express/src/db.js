de456dd14130a9437108a5b96e9dd89e46424951
theweaver19 <theweaver19@protonmail.com>
2021-10-27 17:13:24 -0400

    db

--- express/src/db.js
+++ express/src/db.js
@@ -9,7 +9,7 @@ const knex = require("knex")({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    ssl: process.env.ENVIRONMENT === "dev" ? false : true,
+    ssl: { rejectUnauthorized: false },
   },
 });
 
